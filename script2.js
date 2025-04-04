587effd5f00ca29ec214c0bda063447ea48053e1
MariGaboardi <160857358+MariGaboardi@users.noreply.github.com>
2025-04-04 07:32:20 -0300

    06

--- script2.js
+++ script2.js
@@ -2,3 +2,5 @@ document.addEventListener('DOMContentLoaded', function(){
     const botaoDeAcessibilidade = document.getElementById('botao-acessibilidade');
     const opcoesDeAcessibilidade = document.getElementById('opcoes-acessibilidade');
     let tamanhoAtualFonte = 1;
+    const aumentaFonteBotao = document.getElementById('aumentar-fonte');
+    const diminuiFonteBotao = document.getElementById('diminuir-fonte');
